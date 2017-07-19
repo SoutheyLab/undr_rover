4c47390509b13ea7a07f9e08c8c7990614ca40b9
Khalid Mahmood <mahmood.khalid@gmail.com>
2017-07-19 17:07:56 +1000

    add format and sample field in the vcf file

--- undr_rover/undr_rover.py
+++ undr_rover/undr_rover.py
@@ -554,8 +554,7 @@ def process_blocks(args, blocks, id_info, vcf_file):
             if proportion < args.proportionthresh:
                 var.filter_reason = ''.join([nts(var.filter_reason), ";pt"])
             var.format.extend(''.join(["PCT=", str('{:.2%}'.format(proportion))]))
-            var.gt.extend(''.join([str('{:.2%}'.format(proportion))]))
-
+            var.gt.extend(''.join(["", str('{:.2%}'.format(proportion))]))
             print var
 
             write_variant(vcf_file, var, id_info, args, genotypes)
